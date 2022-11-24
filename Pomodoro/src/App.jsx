caf23af93de43449bab4023de887b4292ade5d75
Colin <colin25.huart@hotmail.com>
2022-11-24 10:14:40 +0100

    initialize

---
+++ Pomodoro/src/App.jsx
@@ -0,0 +1,8 @@
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+    return <div className="text-3xl font-bold underline">Test</div>;
+}
+
+export default App;
